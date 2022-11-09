b62eae1b8423a20be78ac8a774005646de8be0d3
Kheiereddin Boukhatem <105202227+khiereddinbou@users.noreply.github.com>
2022-11-09 12:15:56 +0100

    change api URL

--- admin/src/requestMethods.js
+++ admin/src/requestMethods.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const adminUser = JSON.parse(localStorage.getItem("adminUser")) || {};
 const TOKEN = adminUser?.accessToken;
 
-const BASE_URL = "http://localhost:3005/api/";
+const BASE_URL = "https://khirou-shop-api.onrender.com/api/";
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -5,7 +5,7 @@ import { UserContext } from "./context/UserContext/UserContext";
 const currentUser = JSON.parse(localStorage.getItem("currentUser")) || {};
 const TOKEN = currentUser?.accessToken;
 
-const BASE_URL = "http://localhost:3005/api/";
+const BASE_URL = "https://khirou-shop-api.onrender.com/api/";
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
